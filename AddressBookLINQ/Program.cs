b6af18c35eb2b11b9cec8c42651489121b02902f
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-06-23 16:56:36 +0530

    Added to CreatingTabe

--- AddressBookLINQ/Program.cs
+++ AddressBookLINQ/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace AddressBookLINQ
+﻿using System.Data;
+
+namespace AddressBookLINQ
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("\t\t\t\t\tWelcome to Address Book Program using LINQ\n\n");
+
+            DataTable addressBookTable = new DataTable();
         }
     }
 }
